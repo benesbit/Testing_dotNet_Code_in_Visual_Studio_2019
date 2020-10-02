fbb12738a0a7e8fc43c583cf6b7eebe2e88b10c9
Ben Nesbit <benesbit@yahoo.com>
2020-10-02 13:07:47 -0700

    Create file for testing 'OrderCreationService'

---
+++ WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
+{
+    class OrderCreationServiceTests
+    {
+    }
+}
